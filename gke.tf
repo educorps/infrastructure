4479584ea82b3c99324c8fd3c924c62b24b25ea1
omaramin683 <omaramin683@outlook.com>
2021-09-08 12:23:01 +0100

    gke

--- gke.tf
+++ gke.tf
@@ -20,7 +20,7 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = google_compute_network.vpc_network.network
+  network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.master-gke.name
 }
 
