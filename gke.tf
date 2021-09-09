1c7fb2044a6cb8400f51a3b7f3eeeed95273dc12
omaramin683 <omaramin683@outlook.com>
2021-09-09 09:46:03 +0100

    file_delete

--- gke.tf
+++ gke.tf
@@ -1,18 +1,3 @@
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
-
 resource "google_container_cluster" "primary" {
   project = var.project
   name     = "${var.project}-gke"
--- variables.tf
+++ variables.tf
@@ -6,3 +6,17 @@ variable ip_nodes {}
 variable ip_services {}
 variable ip_pods {}
 variable name {}
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
